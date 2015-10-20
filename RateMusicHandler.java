a86c24ccb3044e9d649b217aabba877d34e7da67
alexherd <alexherd@buffalo.edu>
2015-10-20 14:30:29 -0400

    some changes

--- RateMusicHandler.java
+++ RateMusicHandler.java
@@ -2,9 +2,7 @@ import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.Alert.AlertType;
 
 public class RateMusicHandler implements EventHandler<ActionEvent>{
 
@@ -16,14 +14,6 @@ public class RateMusicHandler implements EventHandler<ActionEvent>{
 		dialogRateMusic.setContentText("Album Name:");
 		Optional<String> rateInput = dialogRateMusic.showAndWait();
 		if (rateInput.isPresent()){
-			//Possibly keep???
-			if(rateInput.get().toLowerCase().equals("ub")){
-				Alert alertUB = new Alert(AlertType.WARNING);
-				alertUB.setTitle("UH-OH SHIT SCHOOL ALERT!");
-				alertUB.setHeaderText("Why do you think it's a bad school?");
-				alertUB.setContentText("We don't think, we know.");
-				alertUB.showAndWait();
-			}
 			//SEARCH FOR ALBUM??
 		}
 	}
